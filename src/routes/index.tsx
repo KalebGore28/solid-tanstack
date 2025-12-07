3fdd8ae73fc7adcbbb7dabbd4e0d7881e614e88f
KalebGore28 <91081805+KalebGore28@users.noreply.github.com>
2025-12-07 13:34:25 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,9 @@ import { createFileRoute } from '@tanstack/solid-router'
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-    // Force a runtime error
-    throw new Error('Test error from App component')
-    return <div>Welcome to the Solid TanStack Starter!</div>
+    return (
+        <div>
+            Welcome to the Solid TanStack Starter!
+        </div>
+    )
 }
